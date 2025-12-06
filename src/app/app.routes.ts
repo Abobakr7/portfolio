a58ac6a0ddc6bdff58f1a9927d802a77d82dc262
Abobakr Osman <mabobakr79@yahoo.com>
2025-12-06 21:07:45 +0200

    feat(app): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './features/home-page/home-page';
+import { BlogPage } from './features/blog-page/blog-page';
+import { WorksPage } from './features/works-page/works-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'blog', component: BlogPage },
+  { path: 'works', component: WorksPage },
+];
