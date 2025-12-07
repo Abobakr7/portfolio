79651a8070e08edc44d8f9fb8e182dd0eef6eab1
Abobakr Osman <mabobakr79@yahoo.com>
2025-12-07 11:40:51 +0200

    feat(routes): add routes' title & add work detail route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 import { HomePage } from './features/home-page/home-page';
 import { BlogPage } from './features/blog-page/blog-page';
 import { WorksPage } from './features/works-page/works-page';
+import { WorkDetailPage } from './features/work-detail-page/work-detail-page';
+import { Work } from './shared/models/work-model';
+import { inject } from '@angular/core';
+import { WorkService } from './shared/services/work-service';
+
+const workResolver: ResolveFn<Work | null> = (route) => {
+  const userService = inject(WorkService);
+  const workId = route.paramMap.get('workId') as string;
+  return userService.getWorkById(parseInt(workId)) ?? null;
+};
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'blog', component: BlogPage },
-  { path: 'works', component: WorksPage },
+  { path: '', component: HomePage, title: 'Home' },
+  { path: 'blog', component: BlogPage, title: 'Blog' },
+  { path: 'works', component: WorksPage, title: 'Featured Works' },
+  { path: 'works/:workId', component: WorkDetailPage, resolve: { workData: workResolver } },
 ];
